9c85b4b4d56bf5c21ae4d18cd0fb1cd4ec867d8f
Eiichi Kaishita <kaishitaeiichi>
2017-05-05 12:49:40 +0900

    update .vimrc

--- bin/bash/setup_vim.sh
+++ bin/bash/setup_vim.sh
@@ -51,9 +51,20 @@ cat <<EOF >> $h/.vimrc
 
 set number
 set tabstop=4
-set expandtab
+set softtabstop=4
 set shiftwidth=4
+set expandtab
+set autoindent
 set smartindent
+
+augroup fileTypeIndent
+    autocmd!
+    autocmd BufNewFile,BufRead *.sh   setlocal tabstop=4 softtabstop=4 shiftwidth=4
+    autocmd BufNewFile,BufRead *.py   setlocal tabstop=4 softtabstop=4 shiftwidth=4
+    autocmd BufNewFile,BufRead *.html setlocal tabstop=4 softtabstop=4 shiftwidth=4
+    autocmd BufNewFile,BufRead *.rb   setlocal tabstop=2 softtabstop=2 shiftwidth=2
+    autocmd BufNewFile,BufRead *.yml  setlocal tabstop=2 softtabstop=2 shiftwidth=2
+augroup END
 
 EOF
 
